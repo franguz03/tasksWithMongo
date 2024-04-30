f22a5ea408b9d49cded37a19183427525e52ca89
franco guzman <francorjuela04@gmail.com>
2024-04-30 13:13:05 -0500

    encriptacion de contraseña con bcrypt y creacion de token con jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,30 +1,36 @@
-import user from "../models/user.model.js"
+import user from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {createAccessToken} from '../libs/jwt.js'
 
-export const register= async(req,res)=>{ //funcion para la ruta register
-const {email, password,userName}=req.body //deconstruimos la peticion
- res.send('registrando')//mandamos esta respuesta
-
-try {
-    const newUser= new user ({ // creamos un objeto
-
-    userName,
-    email,
-    password
- })
-
- await newUser.save()// con esa funcion lo mandamos a la bd
-
-console.log("nuevo usuario",newUser)
-} catch (error) {
-    console.log(error)
+export const register = async (req, res) => {
+  //funcion para la ruta register
+  const { email, password, userName } = req.body; //deconstruimos la peticion
     
-}
-
-
-
-
-}
-export const login=(req,res)=>{ //funcion para la ruta login
-    res.send('login')
+  try {
+    const passwordHash = await bcrypt.hash(password, 10); // devuelve un string aleatorio para encriptar la contraseña
     
-}
+    const newUser = new user({
+      // creamos un objeto
+
+      userName:userName,
+      email:email,
+      password:passwordHash,
+    });
+    console.log(newUser)
+    const userSaved = await newUser.save(); // con esa funcion lo mandamos a la bd
+    const token = await createAccessToken({id:userSaved._id}) // crea un toquen
+    res.cookie('tu token', token)// se guarda en una cookie
+    res.json({
+        id:userSaved._id,
+        email:userSaved.email,
+        name:userSaved.userName
+    });
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+};
+export const login = (req, res) => {
+  //funcion para la ruta login
+  res.send("login");
+};
